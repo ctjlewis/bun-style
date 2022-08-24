4dcc4979ac401cc398a0ccc93758cffe7c5c1447
C. Lewis <ctj.lewis@icloud.com>
2022-08-24 06:40:14 -0500

    chore: touching up

---
+++ test/index.ts
@@ -0,0 +1,9 @@
+import { style } from "../src";
+
+console.log(
+  "\n\n",
+  style(" Running tests... ", ["bold", "underline", "grey"]),
+  "\n\n",
+  style(" ✓ 10 tests passed", ["green"]),
+  "\n\n",
+);
